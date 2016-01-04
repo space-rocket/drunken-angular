7322c09f5f9dfa4651aca4cfbd8bb1b46947ae98
Michael Chavez <bigbassroller@gmail.com>
2016-01-04 08:36:53 -0800

    updated text on blog page

--- client/core/app.component.ts
+++ client/core/app.component.ts
@@ -1,7 +1,28 @@
 import {Component} from 'angular2/core';
 
+interface Post {
+  id: number;
+  title: string;
+}
+
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App boo</h1>'
+  selector: 'my-app',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{post.title}} details!</h2>
+    <div>
+    	<label>id: </label>{{post.id}}
+    </div>
+    <form>
+      <label>title: </label>
+      <input [(ngModel)]="post.title" placeholder="title">
+    </form>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+  public title = 'Blog Posts';
+  public post: Post = {
+    id: 1,
+    title: 'Hello World'
+  };
+}
